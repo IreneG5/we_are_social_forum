cb0a48ef38b7bacd5615be4b7966fc56c6d4b7db
IreneG5 <irene.g5555@gmail.com>
2017-08-09 19:21:58 +0100

    fix email validation in registration form and poll show/hide functionality in the new thread form.

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ class UserRegistrationForm(UserCreationForm):
     MONTH_CHOICES = [(i,i,) for i in xrange(1, 12)]
     YEAR_CHOICES = [(i,i,) for i in xrange(2015, 2036)]
 
-    email = forms.EmailField(label='Email', required=True)
+    #email = forms.EmailField(label='Email', required=True)
     credit_card_number = forms.CharField(label='Credit card number')
     cvv = forms.CharField(label='Security code (CVV)')
     expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES)
@@ -37,6 +37,13 @@ class UserRegistrationForm(UserCreationForm):
             raise forms.ValidationError(message)
 
         return password2
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if not email:
+            message = "Please enter your email address"
+            raise forms.ValidationError(message)
 
     def save(self, commit=True):
         instance = super(UserRegistrationForm, self).save(commit=False)
